b46100df0fedfecfb9132034b9fa8bbc8078bc79
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-09-12 17:39:29 +0200

    Pin where an omitted --output writes in both documents

--- tests/e2e/mod.rs
+++ tests/e2e/mod.rs
@@ -30,6 +30,7 @@ mod headless_support;
 mod headless_tests;
 mod headless_undecided_tests;
 mod install_skills_tests;
+mod instantiate_output_default_tests;
 mod laid_output_root_tests;
 mod list_ready_tests;
 mod loop_budget_refusal_tests;
